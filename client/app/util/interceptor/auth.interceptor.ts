8a3326bb8e90b8537d9b0d7debcc26640d2274f0
Mohamed Meabed <mo.meabed@gmail.com>
2018-01-17 16:28:56 +0100

    Update files

--- client/app/util/interceptor/auth.interceptor.ts
+++ client/app/util/interceptor/auth.interceptor.ts
@@ -15,15 +15,18 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    console.log('intercepted request ... ');
-    const authHeader = this.getAuthorizationHeader();
-    // Clone the request to add the new header.
+    let authHeader = req.headers.get('Authorization');
     let authReq = req;
-    if (authHeader) {
-      authReq = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + authHeader) });
-    }
 
-    console.log('Sending request with new header now ...');
+    if (!authHeader) {
+      console.log('intercepted request ... ');
+      authHeader = this.getAuthorizationHeader();
+      // Clone the request to add the new header.
+      if (authHeader) {
+        authReq = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + authHeader) });
+      }
+      console.log('Sending request with new header now ...');
+    }
 
     // send the newly created request
     return next.handle(authReq)
