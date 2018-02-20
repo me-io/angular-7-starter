70593584d0ee110a8e7751ca33d35519317f7e80
Zeeshan Ahmad <ziishaned@gmail.com>
2018-02-20 16:44:34 +0400

    Add password reset

---
+++ client/app/account/password.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {UserService} from "../services/user.service";
+import {ToastComponent} from "../shared/toast/toast.component";
+
+@Component({
+  selector: 'app-password',
+  templateUrl: './password.component.html',
+  styleUrls: ['./password.component.scss']
+})
+export class PasswordComponent implements OnInit {
+  isLoading = true;
+
+  constructor(private auth: AuthService,
+              public toast: ToastComponent,
+              private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.isLoading = false;
+  }
+
+}
