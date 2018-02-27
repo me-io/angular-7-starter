9819596b6aa4ecc419bab75b7f8bd2e8ff9e3803
Zeeshan Ahmad <ziishaned@gmail.com>
2018-02-27 13:17:01 +0400

    Update e2e dashboard test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
-import { Angular2FullStackPage } from './app.po';
+import {Angular2FullStackPage} from './app.po';
+import {browser, by, element} from "protractor";
 
 describe('angular2-full-stack App', () => {
   let page: Angular2FullStackPage;
@@ -7,11 +8,36 @@ describe('angular2-full-stack App', () => {
     page = new Angular2FullStackPage();
   });
 
+  it('should have a title', () => {
+    page.navigateTo('/');
+
+    expect(page.getTitle()).toEqual('WEB Project');
+  });
+
   it('should display the navbar correctly', () => {
-    page.navigateTo();
+    page.navigateTo('/');
+
     expect(page.getNavbarElement(0)).toEqual('Angular 2 Starter');
     expect(page.getNavbarElement(1)).toEqual('About');
     expect(page.getNavbarElement(2)).toEqual('Register');
     expect(page.getNavbarElement(3)).toEqual('Login');
+  });
+
+  it('should display footer correctly', () => {
+    page.navigateTo('/');
+
+    expect(page.getFooter(0)).toEqual('Copyright © Angular 2 Starter 2018');
+  });
+
+  it('should contain the posts', () => {
+    page.navigateTo('/');
+
+    expect(browser.$$('h5').get(0).getText()).toEqual('Posts');
+  });
+
+  it('should contain the tags', () => {
+    page.navigateTo('/');
+
+    expect(browser.$$('h5').get(1).getText()).toEqual('Tags');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,12 +1,19 @@
 import { $$, browser } from 'protractor';
 
 export class Angular2FullStackPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo($des) {
+    return browser.get($des);
+  }
+
+  getTitle() {
+    return browser.getTitle();
   }
 
   getNavbarElement(n) {
     return $$('app-root a').get(n).getText();
   }
 
+  getFooter(n) {
+    return browser.$$('footer').get(n).getText();
+  }
 }
