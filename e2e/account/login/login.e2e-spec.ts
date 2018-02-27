abdcf22888ce70f0bbd1b2518164f514154911b4
Zeeshan Ahmad <ziishaned@gmail.com>
2018-02-27 16:21:57 +0400

    Update login and register test

--- e2e/account/login/login.e2e-spec.ts
+++ e2e/account/login/login.e2e-spec.ts
@@ -1,6 +1,6 @@
 import {Login} from './login.e2e';
 import {Helper} from "../../helper";
-import {by, element} from "protractor";
+import {$$, by, element} from "protractor";
 
 describe('Login', () => {
   let page: Login;
@@ -29,5 +29,34 @@ describe('Login', () => {
   it('should have password field', function () {
     const emailInput = element(by.id('password')).getAttribute('name');
     expect(emailInput).toBe('password');
+  });
+
+  it('should allow user to change value of email and password inputs', function () {
+    const user = {
+      'email': 'john@info.com',
+      'password': 'johndoe',
+    };
+    const emailInput = element(by.id('email'));
+    const passwordInput = element(by.id('password'));
+
+    emailInput.sendKeys(user.email);
+    passwordInput.sendKeys(user.password);
+
+    expect(emailInput.getAttribute('value')).toBe(user.email);
+    expect(passwordInput.getAttribute('value')).toBe(user.password);
+  });
+
+  it('should display danger alert for wrong credentials', function () {
+    const user = {
+      'email': 'john@info.com',
+      'password': 'johndoe',
+    };
+
+    element(by.id('email')).sendKeys(user.email);
+    element(by.id('password')).sendKeys(user.password);
+
+    element(by.buttonText('Submit')).click();
+
+    expect($$('app-toast .alert').get(0).getText()).toBe("×\nMessage: Username not found");
   });
 });
--- e2e/account/register/register.e2e-spec.ts
+++ e2e/account/register/register.e2e-spec.ts
@@ -30,4 +30,19 @@ describe('Register', () => {
     const emailInput = element(by.id('password')).getAttribute('name');
     expect(emailInput).toBe('password');
   });
+
+  it('should allow user to change value of email and password inputs', function () {
+    const user = {
+      'email': 'john@info.com',
+      'password': 'johndoe',
+    };
+    const emailInput = element(by.id('email'));
+    const passwordInput = element(by.id('password'));
+
+    emailInput.sendKeys(user.email);
+    passwordInput.sendKeys(user.password);
+
+    expect(emailInput.getAttribute('value')).toBe(user.email);
+    expect(passwordInput.getAttribute('value')).toBe(user.password);
+  });
 });
